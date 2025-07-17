91e96a62fe05c9bac132d903180dec2c6e1244ef
KimSeoYeon23 <sollucidus0221@naver.com>
2025-07-17 16:10:48 +0900

    add header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -5,8 +5,13 @@ import Logo from "@/src/shared/ui/Logo/Logo";
 
 const Header = () => {
   return (
-    <div className='p-2 border'>
+    <div className='p-2 border-b flex items-center justify-between w-full'>
       <Logo />
+      <div className='text-sm flex'>
+        <span>Dashbaord</span>
+        <span>Dashbaord</span>
+        <span>Dashbaord</span>
+      </div>
     </div>
   )
 }
